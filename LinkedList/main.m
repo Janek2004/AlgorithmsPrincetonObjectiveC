8725cad643aaa8aa344669797d69c4d17f46fe9e
jan@appzman.com <jan@appzman.com>
2013-10-10 14:37:28 -0500

    Initial Commit

---
+++ LinkedList/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LinkedList
+//
+//  Created by Janusz Chudzynski on 10/10/13.
+//  Copyright (c) 2013 UWF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
